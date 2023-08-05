1dbdaf16dd341c2199fd1e1b293c3f23a9fdaa01
Michael Muthukrishna <michael@muthukrishna.com>
2023-08-05 22:30:31 +0100

    revert

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Linkees, CHANNEL_TYPES } from 'linkees';
+import { Linktree, CHANNEL_TYPES } from 'linkees';
 
 import './css/normalize.css';
 
