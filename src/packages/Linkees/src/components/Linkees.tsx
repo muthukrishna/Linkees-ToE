0b854d367d256dc40316dc70fa370cdfe927f3c4
Michael Muthukrishna <michael@muthukrishna.com>
2023-08-07 16:11:47 +0100

    Update Linkees.tsx
    Remove the header to test something

--- src/packages/Linkees/src/components/Linkees.tsx
+++ src/packages/Linkees/src/components/Linkees.tsx
@@ -25,13 +25,6 @@ function Linkees({
     <div className="App">
       <Header avatar={headerAvatar} name={name} />
       <div className="container row">
-        <a href="https://michael.muthukrishna.com/book" target="_blank" rel="noopener noreferrer">
-          <div className="Card twelve columns" style={{ opacity: 1, transform: 'none' }}>
-            <img className="cover" src={randomImage} alt="" />
-          </div>
-        </a>
-      </div>
-      <div className="container row">
         {cardItems.map((item, i: number) => {
           const coverImage: string =
             item.image ?? CHANNEL_TYPE_VS_COVER_IMAGE[item.type] ?? CHANNEL_TYPE_VS_COVER_IMAGE[CHANNEL_TYPES.WEBSITE];
