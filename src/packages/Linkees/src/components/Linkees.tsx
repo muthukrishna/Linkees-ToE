5334fdc917658d5726742e23a6621be099447aac
Michael Muthukrishna <michael@muthukrishna.com>
2023-08-07 16:15:52 +0100

    Update Linkees.tsx
    Fixed bug. Accidentally deleted header.

--- src/packages/Linkees/src/components/Linkees.tsx
+++ src/packages/Linkees/src/components/Linkees.tsx
@@ -1,3 +1,9 @@
+import React from 'react';
+import Header from './components/Header';
+import Card from './components/Card';
+import Footer from './components/Footer';
+
+import { ItemType } from '../ts';
 import { CHANNEL_TYPE_VS_COVER_IMAGE } from './constant';
 import { CHANNEL_TYPES } from '../constants';
 
@@ -24,6 +30,13 @@ function Linkees({
   return (
     <div className="App">
       <Header avatar={headerAvatar} name={name} />
+      <div className="container row">
+        <a href="https://michael.muthukrishna.com/book" target="_blank" rel="noopener noreferrer">
+          <div className="Card twelve columns" style={{ opacity: 1, transform: 'none' }}>
+            <img className="cover" src={randomImage} alt="" />
+          </div>
+        </a>
+      </div>
       <div className="container row">
         {cardItems.map((item, i: number) => {
           const coverImage: string =
