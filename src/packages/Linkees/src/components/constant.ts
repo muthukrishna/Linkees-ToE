2acd38a2d91c5fa2590ed8128599aea47b9eb7bb
Michael Muthukrishna <michael@muthukrishna.com>
2023-08-07 18:58:53 +0100

    Added logos and details for local bookstores

--- src/packages/Linkees/src/components/constant.ts
+++ src/packages/Linkees/src/components/constant.ts
@@ -9,6 +9,30 @@ import telegram from '../assets/telegram.png';
 import amazon from '../assets/amazon.png';
 import twitter from '../assets/twitter.png';
 
+//bookstores
+import nl1 from '../assets/nl-bookstore1.png';
+import au1 from '../assets/au-bookstore1.png';
+import au2 from '../assets/au-bookstore2.png';
+import basic from '../assets/basicbooks.png';
+import br1 from '../assets/br-bookstore1.png';
+import br2 from '../assets/br-bookstore2.png';
+import ca1 from '../assets/ca-bookstore1.png';
+import ca2 from '../assets/ca-bookstore2.png';
+import de1 from '../assets/de-bookstore1.png';
+import de2 from '../assets/de-bookstore2.png';
+import gb1 from '../assets/gb-bookstore1.png';
+import gb2 from '../assets/gb-bookstore2.png';
+import hachette from '../assets/hachette-global.png';
+import in1 from '../assets/in-bookstore1.png';
+import it1 from '../assets/it-bookstore1.png';
+import it2 from '../assets/it-bookstore2.png';
+import mit from '../assets/MITPress.png';
+import mx1 from '../assets/mx-bookstore1.png';
+import mx2 from '../assets/mx-bookstore2.png';
+import nl2 from '../assets/nl-bookstore2.png';
+import penguin from '../assets/prh-logo.png';
+import sg1 from '../assets/sg-bookstore1.png';
+
 //blurbs
 import henrich from '../assets/website.png';
 import armstrong from '../assets/instagram.png';
@@ -35,9 +59,34 @@ export const CHANNEL_TYPE_VS_COVER_IMAGE = {
   [CHANNEL_TYPES.DRIBBLE]: dribbble,
   [CHANNEL_TYPES.GITHUB]: github,
   [CHANNEL_TYPES.TELEGRAM]: telegram,
-  [CHANNEL_TYPES.AMAZON]: amazon,
   [CHANNEL_TYPES.TWITTER]: twitter,
+  [CHANNEL_TYPES.AMAZON]: amazon,
+  [CHANNEL_TYPES.MIT]: mit,
+  [CHANNEL_TYPES.HACHETTE]: hachette,
+  [CHANNEL_TYPES.BASIC]: basic,
+  [CHANNEL_TYPES.PENGUIN]: penguin,
+  [CHANNEL_TYPES.AU1]: au1,
+  [CHANNEL_TYPES.GB1]: gb1,
+  [CHANNEL_TYPES.DE1]: de1,
+  [CHANNEL_TYPES.CA1]: ca1,
+  [CHANNEL_TYPES.BR1]: br1,
+  [CHANNEL_TYPES.FR1]: fr1,
+  [CHANNEL_TYPES.IT1]: it1,
+  [CHANNEL_TYPES.MX1]: mx1,
+  [CHANNEL_TYPES.NL1]: nl1,
+  [CHANNEL_TYPES.SG1]: sg1,
+  [CHANNEL_TYPES.AU2]: au2,
+  [CHANNEL_TYPES.GB2]: gb2,
+  [CHANNEL_TYPES.DE2]: de2,
+  [CHANNEL_TYPES.CA2]: ca2,
+  [CHANNEL_TYPES.BR2]: br2,
+  [CHANNEL_TYPES.FR2]: fr2,
+  [CHANNEL_TYPES.IT2]: it2,
+  [CHANNEL_TYPES.MX2]: mx2,
+  [CHANNEL_TYPES.NL2]: nl2,
+  [CHANNEL_TYPES.IN1]: in1,
 };
+
 
 export const BLURB_VS_COVER_IMAGE = {
   [BLURBS.HENRICH]: henrich,
--- src/packages/Linkees/src/ts/types.ts
+++ src/packages/Linkees/src/ts/types.ts
@@ -11,7 +11,32 @@ export type KnownChannelTypes =
   | typeof CHANNEL_TYPES.TWITTER
   | typeof CHANNEL_TYPES.WEBSITE
   | typeof CHANNEL_TYPES.AMAZON
-  | typeof CHANNEL_TYPES.YOUTUBE;
+  | typeof CHANNEL_TYPES.YOUTUBE
+  | typeof CHANNEL_TYPES.MIT
+  | typeof CHANNEL_TYPES.HACHETTE
+  | typeof CHANNEL_TYPES.BASIC
+  | typeof CHANNEL_TYPES.PENGUIN
+  | typeof CHANNEL_TYPES.AU1
+  | typeof CHANNEL_TYPES.GB1
+  | typeof CHANNEL_TYPES.DE1
+  | typeof CHANNEL_TYPES.CA1
+  | typeof CHANNEL_TYPES.BR1
+  | typeof CHANNEL_TYPES.FR1
+  | typeof CHANNEL_TYPES.IT1
+  | typeof CHANNEL_TYPES.MX1
+  | typeof CHANNEL_TYPES.NL1
+  | typeof CHANNEL_TYPES.SG1
+  | typeof CHANNEL_TYPES.AU2
+  | typeof CHANNEL_TYPES.GB2
+  | typeof CHANNEL_TYPES.DE2
+  | typeof CHANNEL_TYPES.CA2
+  | typeof CHANNEL_TYPES.BR2
+  | typeof CHANNEL_TYPES.FR2
+  | typeof CHANNEL_TYPES.IT2
+  | typeof CHANNEL_TYPES.MX2
+  | typeof CHANNEL_TYPES.NL2
+  | typeof CHANNEL_TYPES.IN1;
+
 
 export type KnownChannelItem = {
   type: KnownChannelTypes;
